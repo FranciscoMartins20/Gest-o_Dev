02dbabad2b3e2c5abd1c173aed3e749bdc028213
Francisco Martins <jfmartins200@gmail.com>
2024-04-15 18:35:43 +0100

    Imagem

--- FrontEnd/src/scenes/homepage.js
+++ FrontEnd/src/scenes/homepage.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import { Box, Typography, useTheme } from '@mui/material';
+import { keyframes } from '@emotion/react';
+import styled from '@emotion/styled';
+import logoImage from './Logotipo_infodevelop.jpg';
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+const AnimatedImage = styled('img')`
+  animation: ${fadeIn} 2s ease-out;
+  width: 100%; // ajuste conforme necessário
+  max-width: 600px; // limite o tamanho máximo da imagem
+`;
 
 const HomePage = () => {
   const theme = useTheme();
@@ -21,6 +39,7 @@ const HomePage = () => {
       <Typography variant="body1" component="p" textAlign="center">
         Esta é uma homepage simples criada com o tema do Material-UI.
       </Typography>
+      <AnimatedImage src={logoImage} alt="Animated Image" />
     </Box>
   );
 };
