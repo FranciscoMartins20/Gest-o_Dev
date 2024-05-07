2a5a24db7ad16547f859fa28bcc4d9e1ece52145
Francisco Martins <jfmartins200@gmail.com>
2024-05-07 16:51:40 +0100

    Seleção multipla com search

--- BackEnd/db.js
+++ BackEnd/db.js
@@ -3,7 +3,7 @@ const sql = require('mssql');
 const config = {
   user: 'sa',
   password: 'Sql202!s@',
-  server: '192.168.68.94\\sql2017',
+  server: '192.168.68.72\\sql2017',
   database: 'InfoDevelop',
   port: 1433,
   options: {
--- BackEnd/routes/users.js
+++ BackEnd/routes/users.js
@@ -16,10 +16,7 @@ router.post('/login', function(req, res) {
 
 router.post('/logout', usersController.logout);
 router.get('/me', verificarToken, usersController.getUtilizador);
-
-
 router.get('/user/:Username', usersController.getUtilizadorByUsername);
-
 router.get('/getall', usersController.getAllUtilizadores);
 
 module.exports = router;
